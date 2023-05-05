304b3c39cbf64ad813b47014b5fa2d96227d3705
Justin Guo <jjg4qbv@virginia.edu>
2023-05-05 12:07:38 -0400

    modifying setup.bash

--- setup.bash
+++ setup.bash
@@ -6,8 +6,8 @@ npm install
 # return to project directory, replace files in compiler explorer with our changed files
 # this is to circumvent compiler explorer's husky installation, which requires a git log
 cd ..
-mv ./compiler-explorer-changed-files/c++.local.properties ./compiler-explorer/etc/config/c++.local.properties
-mv ./compiler-explorer-changed-files/llvm-ir.ts ./compiler-explorer/lib/llvm-ir.ts
+cp ./compiler-explorer-changed-files/c++.local.properties ./compiler-explorer/etc/config/c++.local.properties
+cp -f ./compiler-explorer-changed-files/llvm-ir.ts ./compiler-explorer/lib/llvm-ir.ts
 npm install
 
 #VSCode debugging
